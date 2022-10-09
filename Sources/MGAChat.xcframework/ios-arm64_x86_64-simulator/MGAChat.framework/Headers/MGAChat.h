73be1c8e865ff7ddf8c9992f3b5d0352b01eebd3
mgabouarab <mgabouarab@gmail.com>
2022-10-10 01:56:14 +0200

    add chat framework

---
+++ Sources/MGAChat.xcframework/ios-arm64_x86_64-simulator/MGAChat.framework/Headers/MGAChat.h
@@ -0,0 +1,18 @@
+//
+//  MGAChat.h
+//  MGAChat
+//
+//  Created by MGAbouarab on 10/10/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MGAChat.
+FOUNDATION_EXPORT double MGAChatVersionNumber;
+
+//! Project version string for MGAChat.
+FOUNDATION_EXPORT const unsigned char MGAChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGAChat/PublicHeader.h>
+
+
--- Sources/MGAChat/MGAChat.swift
+++
@@ -1,5 +0,0 @@
-public struct MGAChat {
-    public static func getInfo() {
-        print("this is our package MGA Caht 😃")
-    }
-}
